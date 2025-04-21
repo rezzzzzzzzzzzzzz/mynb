57155c80de6c8d8a02d6a66808de0d0283c7b3fc
rezzzzzzzzzzzzzz <164221320+rezzzzzzzzzzzzzz@users.noreply.github.com>
2025-04-21 15:04:40 +0330

    Delete content/duckai directory

--- content/duckai/__main__.py
+++
@@ -1,6 +0,0 @@
-"""For using as 'python3 -m duckai'."""
-
-from .cli import cli
-
-if __name__ == "__main__":
-    cli(prog_name="duckai")
--- content/duckai/libs/utils_chat.py
+++
@@ -1,3 +0,0 @@
-class HashBuilder:
-    def build_hash(self, vqd_hash_1: str, headers: dict[str, str]) -> str:
-        return ""
